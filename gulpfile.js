fad6b8c2076c65bacd7b651940f43a8103fe498d
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2022-03-31 10:28:39 +0300

    deplou on ftp + zip packaging

--- gulpfile.js
+++ gulpfile.js
@@ -23,13 +23,15 @@ import { js } from './gulp/tasks/js.js';
 import { images } from './gulp/tasks/images.js';
 import { otfToTtf, ttfToWoff } from './gulp/tasks/fonts.js';
 import { svgSprive } from './gulp/tasks/svgSprive.js';
+import { zip } from './gulp/tasks/zip.js';
+import { ftp } from './gulp/tasks/ftp.js';
 
 
 
 //Наблюдение за изменениями в файлах
 function watcher() {
   gulp.watch(path.watch.files, copy);
-  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.html, html); // gulp.watch(path.watch.html, gulp.series(html, ftp)); заменить на это чтобы динамично обновлять на хостинге измененнные файлы и то же самое проделать для следующих 3-х задач ниже
   gulp.watch(path.watch.scss, scss);
   gulp.watch(path.watch.js, js);
   gulp.watch(path.watch.images, images);
@@ -41,14 +43,21 @@ const fonts = gulp.series(otfToTtf, ttfToWoff);
 
 
 const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
+
 //Сценарии выполнения задач
-const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks);
+const deployZIP = gulp.series(reset, mainTasks, zip);
+const deployFTP = gulp.series(reset, mainTasks, ftp);
+
 
 export { svgSprive }
 export { fonts }
 export { dev }
 export { build }
+export { deployZIP }
+export { deployFTP }
+
 
 //Выполнение сценария по умолчанию
 gulp.task('default', dev);
@@ -56,4 +65,6 @@ gulp.task('default', dev);
 //npm run fonts (конвертирует шрифты в woff woff2)
 //npm run dev == gulp default -режим разработчика
 //npm run build - режим продакшн
+//npm run zip 
+//npm run deploy - выгружаем проект на хостинг в папку test (менять в path.js) 
 
